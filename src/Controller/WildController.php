d62b88847fe0b41d2e22b9a0083357a3c8b8009e
Moxymore67 <mark.alexandre.strasbourg@gmail.com>
2020-05-17 17:10:12 +0200

    Routing

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -10,11 +10,21 @@ class WildController extends AbstractController
 {
     /**
      * @Route("/wild", name="wild_index")
+     * @return Response
      */
     public function index(): Response
     {
-        return $this->render('wild/index.html.twig', [
-            'website' => 'Wild Séries',
-        ]);
+        return $this->render('wild/index.html.twig', ['website' => 'Wild Séries']);
+    }
+
+    /**
+     * @Route("wild/show/{slug<^[a-z0-9-]+$>?0}", name="wild_show_slug")
+     * @param string $slug
+     * @return Response
+     */
+    public function show(string $slug): Response
+    {
+        (empty($slug)) ? $slug = '' : $slug = ucwords(str_replace('-', ' ', $slug));
+        return $this->render('wild/show.html.twig', ['slug' => $slug]);
     }
 }
