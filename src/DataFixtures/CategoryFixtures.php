a9749c2f2b3839499fad34106c9859da9504d526
Moxymore67 <mark.alexandre.strasbourg@gmail.com>
2020-06-08 13:21:14 +0200

    Quest

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -5,16 +5,18 @@ namespace App\DataFixtures;
 use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use Faker;
 
 class CategoryFixtures extends Fixture
 {
+    CONST CATEGORIES = [
+        'Action', 'Aventure', 'Animation', 'Fantastique', 'Horreur', 'Thriller',
+    ];
+
     public function load(ObjectManager $manager)
     {
-        $faker = Faker\Factory::create('fr_FR');
-        for($i=0; $i<=6; $i++) {
+        for($i=0; $i<=5; $i++) {
             $category = new Category();
-            $category->setName($faker->word);
+            $category->setName(self::CATEGORIES[$i]);
             $manager->persist($category);
 
             $this->addReference('categorie_' . $i, $category);
--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -10,6 +10,55 @@ use Faker;
 
 class ProgramFixtures extends Fixture implements DependentFixtureInterface
 {
+    const PROGRAMS = [
+        'Walking Dead' => [
+            'summary' => 'Le policier Rick Grimes se réveille après un long coma. Il découvre avec effarement que le monde, ravagé par une épidémie, est envahi par les morts-vivants.',
+            'category' => 'categorie_4',
+            'country' => 'USA',
+            'year' => 2010,
+        ],
+
+
+        'The Haunting Of Hill House' => [
+            'summary' => 'Plusieurs frères et sœurs qui, enfants, ont grandi dans la demeure qui allait devenir la maison hantée la plus célèbre des États-Unis, sont contraints de se réunir pour finalement affronter les fantômes de leur passé.',
+            'category' => 'categorie_4',
+            'country' => 'USA',
+            'year' => 2010,
+        ],
+
+
+        'American Horror Story' => [
+            'summary' => 'A chaque saison, son histoire. American Horror Story nous embarque dans des récits à la fois poignants et cauchemardesques, mêlant la peur, le gore et le politiquement correct.',
+            'category' => 'categorie_4',
+            'country' => 'USA',
+            'year' => 2010,
+        ],
+
+
+        'Love Death And Robots' => [
+            'summary' => 'Un yaourt susceptible, des soldats lycanthropes, des robots déchaînés, des monstres-poubelles, des chasseurs de primes cyborgs, des araignées extraterrestres et des démons assoiffés de sang : tout ce beau monde est réuni dans 18 courts métrages animés déconseillés aux âmes sensibles.',
+            'category' => 'categorie_4',
+            'country' => 'USA',
+            'year' => 2010,
+        ],
+
+
+        'Penny Dreadful' => [
+            'summary' => 'Dans le Londres ancien, Vanessa Ives, une jeune femme puissante aux pouvoirs hypnotiques, allie ses forces à celles de Ethan, un garçon rebelle et violent aux allures de cowboy, et de Sir Malcolm, un vieil homme riche aux ressources inépuisables. Ensemble, ils combattent un ennemi inconnu, presque invisible, qui ne semble pas humain et qui massacre la population.',
+            'category' => 'categorie_4',
+            'country' => 'USA',
+            'year' => 2010,
+        ],
+
+
+        'Fear The Walking Dead' => [
+            'summary' => 'La série se déroule au tout début de l épidémie relatée dans la série mère The Walking Dead et se passe dans la ville de Los Angeles, et non à Atlanta. Madison est conseillère dans un lycée de Los Angeles. Depuis la mort de son mari, elle élève seule ses deux enfants : Alicia, excellente élève qui découvre les premiers émois amoureux, et son grand frère Nick qui a quitté la fac et a sombré dans la drogue.',
+            'category' => 'categorie_4',
+            'country' => 'USA',
+            'year' => 2010,
+        ],
+    ];
+
     public function getDependencies()
     {
         return [CategoryFixtures::class];
@@ -17,17 +66,19 @@ class ProgramFixtures extends Fixture implements DependentFixtureInterface
 
     public function load(ObjectManager $manager)
     {
+        $i = 0;
         $faker = Faker\Factory::create('fr_FR');
-        for($i=0; $i<=6; $i++) {
+        foreach (self::PROGRAMS as $title => $data) {
             $program = new Program();
-            $program->setTitle( $faker->word . ' ' . $faker->word);
-            $program->setSynopsis($faker->paragraph);
-            $program->setCountry($faker->country);
-            $program->setYear($faker->year);
-            $program->setPoster($faker->imageUrl(1920, 1080));
-            $program->setCategory($this->getReference('categorie_' . $i));
+            $program->setTitle($title);
+            $program->setSynopsis($data['summary']);
+            $program->setCountry($data['country']);
+            $program->setYear($data['year']);
+            $program->setPoster($faker->imageUrl(800,600));
+            $program->setCategory($this->getReference('categorie_0'));
             $manager->persist($program);
             $this->addReference('program_' . $i, $program);
+            $i++;
         }
         $manager->flush();
     }
